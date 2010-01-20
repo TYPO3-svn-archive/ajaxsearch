371de00bfa00b8de0ffc5fcc7cec14be819241b4
commodore <commodore@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-01-20 10:18:29 +0000

    v 0.9.91 - Minor bugfixes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ajaxsearch/trunk@29097 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,44 +1,46 @@
 <?php
-	########################################################################
-	# Extension Manager/Repository config file for ext: "ajaxsearch"
-	#
-	# Auto generated 21-11-2008 11:41
-	#
-	# Manual updates:
-	# Only the data in the array - anything else is removed by next write.
-	# "version" and "dependencies" must not be touched!
-	########################################################################
-	
-	$EM_CONF[$_EXTKEY] = array(
-		'title' => 'AJAX Search Engine',
-		'description' => 'Versatile usable search engine based on AJAX functionality, not only for use with IndexedSearch.',
-		'category' => 'fe',
-		'author' => 'Andre Steiling',
-		'author_email' => 'steiling@elemente.ms',
-		'shy' => '',
-		'dependencies' => '',
-		'conflicts' => '',
-		'priority' => '',
-		'TYPO3_version' => '4.2.1',
-		'PHP_version' => '5.2',
-		'module' => '',
-		'state' => 'beta',
-		'internal' => '',
-		'uploadfolder' => 0,
-		'createDirs' => '',
-		'modify_tables' => '',
-		'clearCacheOnLoad' => 1,
-		'lockType' => '',
-		'author_company' => 'elemente websolutions',
-		'version' => '0.9.9',
-		'constraints' => array(
-			'depends' => array(
-			),
-			'conflicts' => array(
-			),
-			'suggests' => array(
-			),
+
+########################################################################
+# Extension Manager/Repository config file for ext "ajaxsearch".
+#
+# Auto generated 20-01-2010 11:13
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'AJAX Search Engine',
+	'description' => 'Versatile usable search engine based on AJAX functionality, not only for use with IndexedSearch.',
+	'category' => 'fe',
+	'author' => 'Andre Steiling',
+	'author_email' => 'steiling@elemente.ms',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author_company' => 'elemente websolutions',
+	'version' => '0.9.91',
+	'constraints' => array(
+		'depends' => array(
 		),
-		'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"b38c";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"d0fd";s:14:"ext_tables.sql";s:4:"93c9";s:29:"icon_tx_ajaxsearch_config.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"4654";s:7:"tca.php";s:4:"13dd";s:19:"doc/wizard_form.dat";s:4:"ce73";s:20:"doc/wizard_form.html";s:4:"ab8d";}',
-	);
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"b38c";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"b362";s:17:"ext_localconf.php";s:4:"cc6c";s:14:"ext_tables.php";s:4:"d829";s:14:"ext_tables.sql";s:4:"c5a2";s:28:"ext_typoscript_constants.txt";s:4:"707b";s:24:"ext_typoscript_setup.txt";s:4:"9f04";s:15:"flexform_ds.xml";s:4:"bcdd";s:29:"icon_tx_ajaxsearch_config.png";s:4:"b051";s:32:"icon_tx_ajaxsearch_config__h.png";s:4:"3862";s:16:"locallang_db.xml";s:4:"ad36";s:7:"tca.php";s:4:"c0df";s:19:"doc/wizard_form.dat";s:4:"1503";s:20:"doc/wizard_form.html";s:4:"85d0";s:18:"lib/ajaxsearch.php";s:4:"a768";s:17:"lib/locallang.xml";s:4:"8b10";s:31:"pi1/class.tx_ajaxsearch_pi1.php";s:4:"b0ac";s:17:"pi1/locallang.xml";s:4:"2c71";s:23:"res/search_example.html";s:4:"f9c6";s:15:"res/spinner.gif";s:4:"c7b3";s:24:"res/tmpl.ajaxsearch.html";s:4:"4441";}',
+	'suggests' => array(
+	),
+);
+
 ?>
